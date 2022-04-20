cc9346c35ffa5cb1ea2b3df7101477a312750e38
Steven Phan <stevenphan01@gmail.com>
2022-04-20 12:17:34 -0500

    fixed typo when copying rrtstar file

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ mkdir tmp
 cd tmp
 cp ~/workspace/graic-workspace/src/graic_core/src/agent_wrapper.py . 
 cp ~/group_ece484/graic_core/src/baseline.py user_controller.py 
-cp ~/group_ece484/graic_core/src/rrstarr.py .
+cp ~/group_ece484/graic_core/src/rrtstar.py .
 cp ~/group_ece484/graic_core/src/starter.py .
 echo "Finished copying files"
 . ~/workspace/graic-workspace/devel/setup.bash
