52cd3a869441f7ee5fafcd624a8048f87f2d8e56
Steven Phan <stevenphan01@gmail.com>
2022-04-20 12:11:39 -0500

    added a script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!bin/bash 
+cd ..
+mkdir tmp
+cd tmp
+cp ~/workspace/graic-workspace/src/graic_core/src/agent_wrapper.py . 
+cp ~/group_ece484/graic_core/src/baseline.py user_controller.py 
+cp ~/group_ece484/graic_core/src/rrstarr.py .
+cp ~/group_ece484/graic_core/src/starter.py .
+echo "Finished copying files"
+. ~/workspace/graic-workspace/devel/setup.bash
+echo "Running agent_wrapper"
+python3 agent_wrapper.py ego_vehicle
